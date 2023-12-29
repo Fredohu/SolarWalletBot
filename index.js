ae35e4375c941a53dd92ef88d2edab2defe656a7
Fredoo <97898567+Fredohu@users.noreply.github.com>
2023-12-29 04:52:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -44,30 +44,30 @@ bot.on('polling_error', (error) => {
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
     const userId = msg.from.id;
-  
+
     // Check if the user already exists in the database
     let user = await User.findOne({ telegramId: userId });
-  
+
     if (!user) {
-      // Create a Solana wallet for the user
-      const keypair = Keypair.generate();
-      const publicKey = keypair.publicKey.toString();
-      const privateKey = keypair.secretKey.toString();
-  
-      // Store the user in the database
-      user = new User({
-        telegramId: userId,
-        solanaPublicKey: publicKey,
-        solanaPrivateKey: privateKey,
-      });
-      await user.save();
-  
-      bot.sendMessage(chatId, `Welcome to SolarBot! Your Solana wallet has been created.\nYour public key: ${publicKey}`);
-      bot.sendMessage(chatId, `Keep your private key secure. If lost, it cannot be recovered.\nYour encrypted private key: ${privateKey}`);
+        // Create a Solana wallet for the user
+        const keypair = Keypair.generate();
+        const publicKey = keypair.publicKey.toString();
+        const privateKey = keypair.secretKey.toString();
+
+        // Store the user in the database
+        user = new User({
+            telegramId: userId,
+            solanaPublicKey: publicKey,
+            solanaPrivateKey: privateKey,
+        });
+        await user.save();
+
+        bot.sendMessage(chatId, `Welcome to SolarBot! Your Solana wallet has been created.\nYour public key: ${publicKey}`);
+        bot.sendMessage(chatId, `Keep your private key secure. If lost, it cannot be recovered.\nYour encrypted private key: ${privateKey}`);
     } else {
-      bot.sendMessage(chatId, `Welcome back! Your Solana wallet already exists.\nYour public key: ${user.solanaPublicKey}`);
+        bot.sendMessage(chatId, `Welcome back! Your Solana wallet already exists.\nYour public key: ${user.solanaPublicKey}`);
     }
-  });
+});
   
 // Function to create a new token, mint tokens, set metadata, set authority, and store in the database
 async function createToken(tokenDetails, userPublicKey, chatId) {
@@ -125,51 +125,51 @@ async function createToken(tokenDetails, userPublicKey, chatId) {
 bot.onText(/\/deploy/, async (msg) => {
     const chatId = msg.chat.id;
     const userId = msg.from.id;
-  
+
     // Retrieve user from the database
     const user = await User.findOne({ telegramId: userId });
-  
+
     if (user) {
-      // Object to store token details
-      const tokenDetails = {};
-  
-      // Function to ask for token details
-      const askForTokenDetails = async (fieldName, prompt) => {
-        bot.sendMessage(chatId, prompt);
-        return new Promise((resolve) => {
-          bot.once('text', async (msg) => {
-            tokenDetails[fieldName] = msg.text;
-            resolve();
-          });
-        });
-      };
-  
-      // Function to handle photo messages
-      const askForLogo = async () => {
-        return new Promise(async (resolve) => {
-          bot.sendMessage(chatId, '3. Upload Token Logo (Accepted formats: .png, .jpg, .jpeg):');
-  
-          bot.once('photo', async (msg) => {
-            const fileId = msg.photo[msg.photo.length - 1].file_id;
-  
-            // Get file details
-            const fileDetails = await bot.getFile(fileId);
-  
-            // Check file format
-            const fileName = fileDetails.file_path;
-            if (fileName.endsWith('.png') || fileName.endsWith('.jpg') || fileName.endsWith('.jpeg')) {
-              tokenDetails['logo'] = fileName;
-              bot.sendMessage(chatId, 'Logo uploaded successfully.');
-            } else {
-              bot.sendMessage(chatId, 'Invalid file format. Please upload a .png, .jpg, or .jpeg file.');
-              await askForLogo(); // Ask again for logo
-            }
-            resolve();
-          });
-        });
-      };
-  
-      // Ask the user for token details sequentially
+        // Object to store token details
+        const tokenDetails = {};
+
+        // Function to ask for token details
+        const askForTokenDetails = async (fieldName, prompt) => {
+            bot.sendMessage(chatId, prompt);
+            return new Promise((resolve) => {
+                bot.once('text', async (msg) => {
+                    tokenDetails[fieldName] = msg.text;
+                    resolve();
+                });
+            });
+        };
+
+        // Function to handle photo messages
+        const askForLogo = async () => {
+            return new Promise(async (resolve) => {
+                bot.sendMessage(chatId, '3. Upload Token Logo (Accepted formats: .png, .jpg, .jpeg):');
+
+                bot.once('photo', async (msg) => {
+                    const fileId = msg.photo[msg.photo.length - 1].file_id;
+
+                    // Get file details
+                    const fileDetails = await bot.getFile(fileId);
+
+                    // Check file format
+                    const fileName = fileDetails.file_path;
+                    if (fileName.endsWith('.png') || fileName.endsWith('.jpg') || fileName.endsWith('.jpeg')) {
+                        tokenDetails['logo'] = fileName;
+                        bot.sendMessage(chatId, 'Logo uploaded successfully.');
+                    } else {
+                        bot.sendMessage(chatId, 'Invalid file format. Please upload a .png, .jpg, or .jpeg file.');
+                        await askForLogo(); // Ask again for logo
+                    }
+                    resolve();
+                });
+            });
+        };
+
+        // Ask the user for token details sequentially
         await askForTokenDetails('name', '1. Enter Token Name:');
         await askForTokenDetails('symbol', '2. Enter Token Symbol:');
         await askForLogo(); // Ask for Token Logo
@@ -181,33 +181,32 @@ bot.onText(/\/deploy/, async (msg) => {
 
         // Display collected token details for confirmation
         const confirmationMessage = `Please confirm the following details:\n\n`
-        + `Token Name: ${tokenDetails.name}\n`
-        + `Token Symbol: ${tokenDetails.symbol}\n`
-        + `Token Logo: ${tokenDetails.logo}\n`
-        + `Token Description: ${tokenDetails.description}\n`
-        + `Token Decimal: ${tokenDetails.decimal}\n`
-        + `Token Supply: ${tokenDetails.amount}\n\n`
-        + `Type /confirm to proceed or /cancel to abort.`;
+            + `Token Name: ${tokenDetails.name}\n`
+            + `Token Symbol: ${tokenDetails.symbol}\n`
+            + `Token Logo: ${tokenDetails.logo}\n`
+            + `Token Description: ${tokenDetails.description}\n`
+            + `Token Decimal: ${tokenDetails.decimal}\n`
+            + `Token Supply: ${tokenDetails.amount}\n\n`
+            + `Type /confirm to proceed or /cancel to abort.`;
 
         bot.sendMessage(chatId, confirmationMessage);
-  
-      // Wait for user confirmation
-    bot.once('text', async (msg) => {
-        const confirmationCommand = msg.text.toLowerCase();
 
-        if (confirmationCommand === '/confirm') {
-            // Call the createToken function with the collected token details
-            await createToken(tokenDetails, user.solanaPublicKey, chatId);
-          // ...
-  
-          bot.sendMessage(chatId, 'Token deployment in progress...');
-        } else if (confirmationCommand === '/cancel') {
-          bot.sendMessage(chatId, 'Token deployment canceled.');
-        } else {
-          bot.sendMessage(chatId, 'Invalid command. Token deployment canceled.');
-        }
-      });
+        // Wait for user confirmation
+        bot.once('text', async (msg) => {
+            const confirmationCommand = msg.text.toLowerCase();
+
+            if (confirmationCommand === '/confirm') {
+                // Call the createToken function with the collected token details
+                await createToken(tokenDetails, user.solanaPublicKey, chatId);
+
+                bot.sendMessage(chatId, 'Token deployment in progress...');
+            } else if (confirmationCommand === '/cancel') {
+                bot.sendMessage(chatId, 'Token deployment canceled.');
+            } else {
+                bot.sendMessage(chatId, 'Invalid command. Token deployment canceled.');
+            }
+        });
     } else {
-      bot.sendMessage(chatId, 'Welcome to SolarBot! Please use /start to create a Solana wallet first.');
+        bot.sendMessage(chatId, 'Welcome to SolarBot! Please use /start to create a Solana wallet first.');
     }
-  });
+});
